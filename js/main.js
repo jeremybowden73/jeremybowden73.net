49150934c7fc9836d9d368c06ed3fb2402c27fe1
jeremybowden73 <jeremybowden73.gmail.com>
2018-08-14 19:22:51 +0100

    Corrected display issues with <section>s

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-// const menu = document.getElementById("menu");
 
-// menu.addEventListener("click", () => {
-//   menu.style.color = "red";
+// event handler for the menu button on mobile devices
+const mobileMenu = document.getElementById("mobile-menu");
+
+const menuIcon = document.getElementById("mobile-menu-burger");
+
+menuIcon.addEventListener("click", function () {
+  if (menuIcon.classList.contains("open")) {
+    mobileMenu.classList.add("hidden");
+    menuIcon.classList.remove("open");
+  } else {
+    mobileMenu.classList.remove("hidden");
+    menuIcon.classList.add("open");
+  }
+});
+// $(document).ready(function () {
+//   $('.menu-icon').click(function () {
+//     $(this).toggleClass('open');
+//   });
 // });
 
-$(document).ready(function () {
-  $('.menu-icon').click(function () {
-    $(this).toggleClass('open');
-  });
-});
