9fa36b7a59f4b28919e6a89e2445958dc3a7b408
jeremybowden73 <jeremybowden73.gmail.com>
2018-08-15 14:20:54 +0100

    Added more sections

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
-
-// event handler for the menu button on mobile devices
-const mobileMenu = document.getElementById("mobile-menu");
-const menuIcon = document.getElementById("mobile-menu-burger");
-
-menuIcon.addEventListener("click", function () {
-  if (menuIcon.classList.contains("open")) {
-    menuIcon.classList.remove("open");
-    mobileMenu.classList.add("hidden");
+// event handler for the menu icon (only narrow screens will see this)
+$("#mobile-menu-burger").click(function () {
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
+    $("#mobile-menu").addClass("hidden");
   } else {
-    menuIcon.classList.add("open");
-    mobileMenu.classList.remove("hidden");
+    $(this).addClass("open");
+    $("#mobile-menu").removeClass("hidden");
   }
 });
 
-// event handler for the menu options on mobile devices
-mobileMenu.addEventListener("click", function () {
-  mobileMenu.classList.add("hidden");
-  menuIcon.classList.remove("open");
+// event handler for closing the menu (only narrow screens will see this)
+$("#mobile-menu").click(function () {
+  $(this).addClass("hidden");
+  $("#mobile-menu-burger").removeClass("open");
 });
 
+// to do ... apply DRY to these 4 event handlers
+// which scroll to the appropriate section when clicked on via the menu
+$("#linkTo1").click(function () {
+  $('html, body').animate({
+    scrollTop: $("#section1").offset().top - 80
+  }, 1000);
+});
+
+$("#linkTo2").click(function () {
+  $('html, body').animate({
+    scrollTop: $("#section2").offset().top - 80
+  }, 1000);
+});
+
+$("#linkTo3").click(function () {
+  $('html, body').animate({
+    scrollTop: $("#section3").offset().top - 80
+  }, 1000);
+});
+
+$("#linkTo4").click(function () {
+  $('html, body').animate({
+    scrollTop: $("#section4").offset().top - 80
+  }, 1000);
+});
 
 
 
